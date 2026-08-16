e0ee30bca635a7461fbed105c97df201156cd685
irfanpc <irfanpc@example.com>
2026-08-16 19:38:09 +0530

    Implement PWA Service Worker for force auto-reloading

--- www/OneSignalSDKWorker.js
+++ www/OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
+// Import OneSignal's worker
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// Import Angular's PWA Service Worker for asset caching and auto-updates
+importScripts("ngsw-worker.js");
