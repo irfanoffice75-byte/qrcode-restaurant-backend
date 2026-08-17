52a6efc46d767091883a606fc7b9f51fdffe2629
irfanpc <irfanpc@example.com>
2026-08-17 10:54:13 +0530

    Fix OOM by switching back to zipped www deploy

--- www/OneSignalSDKWorker.js
+++
@@ -1,5 +0,0 @@
-// Import OneSignal's worker
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
-
-// Import Angular's PWA Service Worker for asset caching and auto-updates
-importScripts("ngsw-worker.js");
